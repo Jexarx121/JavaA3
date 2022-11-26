ef4735146307ee457f65e0239ccc1150e3a2e58d
Jexarx121 <zjchen402@gmail.com>
2022-11-26 12:58:29 +0000

    Refactored all tests to add in display names for each test. Also created a beforeEach method that creates colours objects, minimising some code.
    Lastly reduced the number of assertions and confirmed they all still work properly.

--- src/test/java/Colour/ColourTest.java
+++ src/test/java/Colour/ColourTest.java
@@ -1,5 +1,6 @@
 package Colour;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,15 +8,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ColourTest {
 
-    @Test
-    public void testThreeParameterColour() {
-        Colour colour = new Colour(1, 1, 1);
-        assertTrue(colour.getMin() <= colour.getRedColourValue() && colour.getRedColourValue() <= colour.getMax());
-        assertTrue(colour.getMin() <= colour.getBlueColourValue() && colour.getBlueColourValue() <= colour.getMax());
-        assertTrue(colour.getMin() <= colour.getGreenColourValue() && colour.getGreenColourValue() <= colour.getMax());
+    Colour colour1;
+    Colour colour2;
+    Colour colour3;
+    @BeforeEach
+    public void setup() {
+        colour1 = new Colour(1, 1, 1);
+        colour2 = new Colour(16777215);
+        colour3 = new Colour(14561);
     }
 
     @Test
+    @DisplayName("Creating a colour through constructor with 3 parameters.")
+    public void testThreeParameterColour() {
+        assert colour1 != null;
+    }
+
+    @Test
+    @DisplayName("Creating 3 parameters colours that should throw exceptions.")
     public void testThreeParameterColourException() {
         Throwable exceptionRed = assertThrows(IllegalArgumentException.class, () ->
                 new Colour(2, 1, 1));
@@ -24,24 +34,32 @@ class ColourTest {
         Throwable exceptionGreen = assertThrows(IllegalArgumentException.class, () ->
                 new Colour(1, 2, 1));
 
-        assertEquals("Red value should be between 0 and 1 or 255.", exceptionRed.getMessage());
-        assertEquals("Blue value should be between 0 and 1 or 255.", exceptionBlue.getMessage());
-        assertEquals("Green value should be between 0 and 1 or 255.", exceptionGreen.getMessage());
+        assertAll(
+                () ->  assertEquals("Red value should be between 0 and 1 or 255.", exceptionRed.getMessage()),
+                () ->  assertEquals("Blue value should be between 0 and 1 or 255.", exceptionBlue.getMessage()),
+                () -> assertEquals("Green value should be between 0 and 1 or 255.", exceptionGreen.getMessage())
+        );
     }
 
     @Test
+    @DisplayName("Creating a colour with single parameter constructor.")
     public void testSingleParameterColour() {
-        Colour colour = new Colour(14561);
-        assertTrue(colour.getMin() <= colour.getRedColourValue() && colour.getRedColourValue() <= colour.getMax());
-        assertTrue(colour.getMin() <= colour.getBlueColourValue() && colour.getBlueColourValue() <= colour.getMax());
-        assertTrue(colour.getMin() <= colour.getGreenColourValue() && colour.getGreenColourValue() <= colour.getMax());
+        assert colour3 != null;
     }
 
     @Test
+    @DisplayName("Creating single parameter colour that should throw an exception.")
     public void testSingleParameterColourException() {
         Throwable exceptionLength = assertThrows(IllegalArgumentException.class, () ->
                 new Colour(1000000000));
 
-        assertEquals("RGB value should be between 0 and 16777215", exceptionLength.getMessage());
+        assertEquals("RGB value should be between 0 and 16777215.", exceptionLength.getMessage());
+    }
+
+    @Test
+    @DisplayName("Comparing colours' rgb values.")
+    public void testEqualsColours() {
+        assertTrue(colour1.equals(colour2));
+        assertFalse(colour2.equals(colour3));
     }
 }
